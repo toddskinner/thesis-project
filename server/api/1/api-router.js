f2ad68f0e0de0cd32ad5c8a057949bc70404ecd7
Todd Skinner <todd.skinner@gmail.com>
2015-02-21 09:30:44 -0800

    FIX adds use of apiRouter to our API requests

--- server/api/1/api-router.js
+++ server/api/1/api-router.js
@@ -26,18 +26,18 @@ apiRouter.get('/odeskData', function(req,res) {
 // Redirect the user to the OAuth 2.0 provider for authentication.  When
 // complete, the provider will redirect the user back to the application at
 //     /auth/provider/callback
-app.get('/auth/elance', passport.authenticate('elance'));
-app.get('/auth/odesk', passport.authenticate('odesk'));
+apiRouter.get('/auth/elance', passport.authenticate('elance'));
+apiRouter.get('/auth/odesk', passport.authenticate('odesk'));
 
 // The OAuth 2.0 provider has redirected the user back to the application.
 // Finish the authentication process by attempting to obtain an access
 // token.  If authorization was granted, the user will be logged in.
 // Otherwise, authentication has failed.
-app.get('/auth/elance/callback', 
-  passport.authenticate('elance', { successRedirect: '/#/selectSubject',
-                                      failureRedirect: '/login' }));
-app.get('/auth/odesk/callback', 
-  passport.authenticate('odesk', { successRedirect: '/#/selectSubject',
-                                      failureRedirect: '/login' }));
+apiRouter.get('/auth/elance/callback', 
+  passport.authenticate('elance', { successRedirect: '/about',
+                                      failureRedirect: '/contact' }));
+apiRouter.get('/auth/odesk/callback', 
+  passport.authenticate('odesk', { successRedirect: '/about',
+                                      failureRedirect: '/contact' }));
 
 module.exports = apiRouter;
